725563b07b11a8900d787a06b631ca8f64902b77
komalgt13 <komal1@cs.iitr.ac.in>
2024-03-10 19:43:49 +0530

    changes made to the optimise_debt.js

--- server/utils/optimise_debts.js
+++ server/utils/optimise_debts.js
@@ -1,44 +1,52 @@
-function minTransfers(transactions) {
-    const balancesMap = new Map();
-    
-    // Calculate the overall balance for each member
-    for (const transaction of transactions) {
-        const from = transaction[0];
-        const to = transaction[1];
-        const amount = transaction[2];
-        balancesMap.set(from, (balancesMap.get(from) || 0) - amount);
-        balancesMap.set(to, (balancesMap.get(to) || 0) + amount);
+    var N = 3;
+    function getMin(arr)
+    {
+        var minInd = 0;
+        for (i = 1; i < N; i++)
+            if (arr[i] < arr[minInd])
+                minInd = i;
+        return minInd;
     }
-    
-    const balances = Array.from(balancesMap.values()).filter(balance => balance !== 0);
-    const transactionsList = [];
-    
-    const minTransactionCount = settleDebts(balances, 0, transactionsList);
-    
-    return { minTransactionCount, transactionsList };
-}
+     
+    function getMax(arr)
+    {
+        var maxInd = 0;
+        for (i = 1; i < N; i++)
+            if (arr[i] > arr[maxInd])
+                maxInd = i;
+        return maxInd;
+    }
 
-function settleDebts(balanceList, currentIndex, transactionsList) {
-    if (balanceList.length === 0 || currentIndex >= balanceList.length) return 0;
-    if (balanceList[currentIndex] === 0) {
-        return settleDebts(balanceList, currentIndex + 1, transactionsList);
+    function minOf2(x , y)
+    {
+        return (x < y) ? x: y;
     }
-    
-    const currentBalance = balanceList[currentIndex];
-    let minTransactionCount = Number.MAX_SAFE_INTEGER;
-    
-    for (let nextIndex = currentIndex + 1; nextIndex < balanceList.length; nextIndex++) {
-        const nextBalance = balanceList[nextIndex];
-        if (currentBalance * nextBalance < 0) {
-            balanceList[nextIndex] = currentBalance + nextBalance;
-            transactionsList.push({ from: currentIndex, to: nextIndex, amount: Math.abs(nextBalance) });
-            minTransactionCount = Math.min(minTransactionCount, 1 + settleDebts(balanceList, currentIndex + 1, transactionsList));
-            balanceList[nextIndex] = nextBalance;
-            if (currentBalance + nextBalance === 0) {
-                break;
-            }
-        }
+
+    function minCashFlowRec(amount)
+    {
+        var mxCredit = getMax(amount), mxDebit = getMin(amount);
+        if (amount[mxCredit] == 0 && amount[mxDebit] == 0)
+            return;
+        var min = minOf2(-amount[mxDebit], amount[mxCredit]);
+        amount[mxCredit] -= min;
+        amount[mxDebit] += min;
+  
+        document.write("<br>Person " + mxDebit + " pays " + min
+                                + " to " + "Person " + mxCredit);
+        minCashFlowRec(amount);
     }
-    
-    return minTransactionCount;
-}
+     
+    function minCashFlow(graph)
+    {
+        var amount=Array.from({length: N}, (_, i) => 0);
+        for (p = 0; p < N; p++)
+        for (i = 0; i < N; i++)
+            amount[p] += (graph[i][p] - graph[p][i]);
+     
+        minCashFlowRec(amount);
+    }
+
+    var graph = [ [0, 1000, 2000],
+                        [0, 0, 5000],
+                        [0, 0, 0]];
+    minCashFlow(graph);
